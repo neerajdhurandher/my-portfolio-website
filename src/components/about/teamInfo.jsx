54883edad2dc5d43e483d6bf5a6a12c1b6df1209
neerajdhurandher01 <86510313+neerajdhurandher01@users.noreply.github.com>
2021-07-17 23:20:32 +0530

    bug fix

--- src/components/about/teamInfo.jsx
+++ src/components/about/teamInfo.jsx
@@ -7,9 +7,7 @@ const about = () => (
     <div>
       <h4 className="font30 weight800">Drummer</h4>
       <h4 className="font30 weight800">Video Editor</h4>
-      <p className="font12 weight500">
-       Lorem ipsum dolor sit amet consectetur adipisicing elit. Error, debitis voluptate! Totam suscipit eos paria.
-      </p>
+      <p className="font12 weight500"> Drumming is my hobby , I love to play Drums in my free time. I am playing Drums since my chilhood. Apart from this I like to shoot videos and play with them. </p>
       <div className="some-heading-half-display margin-center" >
           <line style={{textAlign:"center"}}>Check out &nbsp;</line>
         </div>
