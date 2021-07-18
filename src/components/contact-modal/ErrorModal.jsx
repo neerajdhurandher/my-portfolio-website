48d52db636808c79b1518982144e2d013a319727
neerajdhurandher01 <86510313+neerajdhurandher01@users.noreply.github.com>
2021-07-18 11:48:39 +0530

    Change Para of Send Message Error

--- src/components/contact-modal/ErrorModal.jsx
+++ src/components/contact-modal/ErrorModal.jsx
@@ -4,8 +4,9 @@ import "./modal.scss";
 const errorModal = (props) => (
     <div className="error__modal">
       <p>Oooops!!!</p>
-      <p>
-        Something went wrong :{"("}</p>
+      {/* <p>Something went wrong :{"("}</p> */}
+
+        <p>This Service is not Available :{"("} , We will Update it Soon.</p>
       <div role="button" tabIndex={0} className="modal__btn flex-center" onClick={props.closeModal} onKeyDown={props.closeModal}>
         <p>Ok</p>
       </div>
