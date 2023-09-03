f6b7b6d564b0346e18a555219a6582f52fb072f9
neerajdhurandher-deloitte <101085508+neerajdhurandher-deloitte@users.noreply.github.com>
2023-09-03 17:10:36 +0530

    rename resume file, add instagram account link in about section & download icon in download btn and code format

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -6,39 +6,41 @@ import "./about.scss";
 
 // Components
 import EduWorkBox from './eduWorkBox';
-import OtherIntrestInfo from "./itemInfo";
+import OtherInterestInfo from "./itemInfo";
 import EventsScrollView from "../events/eventSwiper";
 import AboutDetails from "./aboutdetails";
-import CustomButton from "../ui-components/button/button";
 import Title from "../ui-components/title/title";
+import DownloadIcon from '@material-ui/icons/ArrowDownwardRounded';
+
 // Assets
-import hashedinlogo from "../../assets/about/hashedin_logo.png"
-import atglogo from "../../assets/about/atg_logo.png";
-import rungtalogo from "../../assets/about/rungta_logo.png";
-import shakuntalalogo from "../../assets/about/shakuntala_logo.png";
-import Resume from "../../assets/about/Neeraj _Dhurandher_Resume.pdf"
+import hashedin_logo from "../../assets/about/hashedin_logo.png";
+import atg_logo from "../../assets/about/atg_logo.png";
+import rungta_logo from "../../assets/about/rungta_logo.png";
+import shakuntala_logo from "../../assets/about/shakuntala_logo.png";
+import resume from "../../assets/about/neeraj_dhurandher_resume.pdf";
 
-let emaillink = "mailto:websiteneeraj@gmail.com?subject = Feedback From Website&body = Message";
-let phonelink = "tel:7224010437";
-let homelink = "https://goo.gl/maps/jQrLPbbSMJz7MiFx6";
 
+// links
+let email_link = "mailto:websiteneeraj@gmail.com?subject = Feedback From Website&body = Message";
+let home_link = "https://goo.gl/maps/jQrLPbbSMJz7MiFx6";
+let instagram_link = "https://instagram.com/neeraj_dhurandher"
+
+// content
 let drummer_title = "Drummer";
-let drummer_des = "Drumming is my hobby , I love to play drums in my free time. I am playing drums since my chilhood."
+let drummer_des = "Playing drums has always been my favorite hobby. I've been pounding on the drums ever since I was a kid, and it remains my go-to activity in my spare time."
 
 
 const about = () => (
   <div id="about">
 
     <div className="wrapper">
-
       <Title title="ABOUT ME" />
-
       <div>
-        {/* <p className="font20 download_cv_text"> Download my CV </p> */}
-        <div className="resume_dow_btn text-center"  >
-          <a href={Resume} download = "Neeraj Dhurandher Resume" >
+        <div className="resume_dow_btn text-center flex-center">
+          <a href={resume} download="Neeraj Dhurandher Resume">
             Download Resume
           </a>
+          <DownloadIcon className="download_icon"/>
         </div>
       </div>
 
@@ -46,18 +48,15 @@ const about = () => (
         <p style={{ color: "black", fontSize: "3px" }}>.</p>
       </div>
 
-
-      {/* yellow box with contact details */}
       <div>
         <div className="box-yellow-border contact-box" >
-
           <Row>
             <Col md={12} lg={6}>
-              <Fade left duration = {1000}>
+              <Fade left duration={1000}>
                 <div>
-                  <AboutDetails type="email" title="Email " value="websiteneeraj@gmail.com " link={emaillink} />
+                  <AboutDetails type="email" title="Email " value="websiteneeraj@gmail.com" link={email_link} />
                   <p style={{ color: "transparent" }} >lorem</p>
-                  <AboutDetails type="phone" title="Phone No. " value="7224010437" link={phonelink} />
+                  <AboutDetails type="instagram" title="Instagram" value="neeraj_dhurandher" link={instagram_link} />
                   <p style={{ color: "transparent" }} >lorem</p>
                 </div>
               </Fade>
@@ -65,11 +64,11 @@ const about = () => (
 
 
             <Col md={12} lg={6}>
-              <Fade right duration = {1000}>
+              <Fade right duration={1000}>
                 <div >
                   <AboutDetails type="website" title="Website " value="neerajdhurandher.me" link="https://neerajdhurandher.me/" />
                   <p style={{ color: "transparent" }} >lorem</p>
-                  <AboutDetails type="address" title="Address " value="Bhilai (CG), India" link={homelink} />
+                  <AboutDetails type="address" title="Address " value="Bhilai (CG), India" link={home_link} />
                   <p style={{ color: "transparent" }} >lorem</p>
                 </div>
               </Fade>
@@ -85,36 +84,35 @@ const about = () => (
         <p className="some-heading-full-display">Work Experience</p>
       </div>
 
-      <Fade left duration = {1000}>
-        <EduWorkBox avatar={hashedinlogo} name="Hashedin by Deloitte, Bengaluru" name_last="" job="SDET" year="MAR 2022 - Present" link="https://hashedin.com/" />
+      <Fade left duration={1000}>
+        <EduWorkBox avatar={hashedin_logo} name="Hashedin by Deloitte, Bengaluru" name_last="" job="SDET" year="MAR 2022 - Present" link="https://hashedin.com/" />
       </Fade>
 
-      <Fade right duration = {1000}>
-        <EduWorkBox avatar={atglogo} name="Across The Globe (ATG) , Bengaluru" name_last="" job="Android Developer Intern " year="JUN - DEC 2021" link="https://www.atg.world/" />
+      <Fade right duration={1000}>
+        <EduWorkBox avatar={atg_logo} name="Across The Globe (ATG) , Bengaluru" name_last="" job="Android Developer Intern " year="JUN - DEC 2021" link="https://www.atg.world/" />
       </Fade>
 
       <div>
         <p className="some-heading-full-display">Education</p>
       </div>
 
-      <Fade left duration = {1000}>
-        <EduWorkBox avatar={rungtalogo} name="Rungta College Of Engineering" name_last="and Technology , Bhilai" job="B.E. (CSE)" year="2018-2022" link="https://www.rungta.ac.in/" />
+      <Fade left duration={1000}>
+        <EduWorkBox avatar={rungta_logo} name="Rungta College Of Engineering" name_last="and Technology , Bhilai" job="B.E. (CSE)" year="2018-2022" link="https://www.rungta.ac.in/" />
       </Fade>
-      <Fade right duration = {1000}>
-        <EduWorkBox avatar={shakuntalalogo} name="Shakuntala Vidyalaya , Bhilai" name_last="" job="12th & 10th" year="2018 & 2016" link="http://www.shakuntalavidyalaya.edu.in/" />
+      <Fade right duration={1000}>
+        <EduWorkBox avatar={shakuntala_logo} name="Shakuntala Vidyalaya , Bhilai" name_last="" job="12th & 10th" year="2018 & 2016" link="http://www.shakuntalavidyalaya.edu.in/" />
       </Fade>
-
 
       <div>
-        <p className="some-heading-full-display">Other Intrests</p>
+        <p className="some-heading-full-display">Other Interests</p>
       </div>
 
     </div>
 
-    <div className="other-intrest-sec">
+    <div className="other-interest-sec">
       <Row>
         <Col xs={6} md={4}>
-          <OtherIntrestInfo title = {drummer_title} des = {drummer_des}/>
+          <OtherInterestInfo title={drummer_title} des={drummer_des} />
         </Col>
         <Col xs={12} md={8}>
           <EventsScrollView />
--- src/components/about/aboutdetails.jsx
+++ src/components/about/aboutdetails.jsx
@@ -1,39 +1,31 @@
 import React from "react";
-import { Row, Col } from "react-flexbox-grid";
 import "./aboutdetails.scss";
 
 // /Icon
 import MailOutlineRoundedIcon from '@material-ui/icons/MailOutlineRounded';
-import PhoneInTalkOutlinedIcon from '@material-ui/icons/PhoneInTalkOutlined';
 import LanguageOutlinedIcon from '@material-ui/icons/LanguageOutlined';
 import HomeOutlinedIcon from '@material-ui/icons/HomeOutlined';
+import InstagramIcon from '@material-ui/icons/Instagram';
 
-
-let detail_type = "phone_number";
-
-function DetailType (props){ 
-    if(props.type == "email")
-  return  <MailOutlineRoundedIcon className="about_details_icon" style={{fontSize:"25px"}}/>;
-  if(props.type == "phone")
-  return  <PhoneInTalkOutlinedIcon className="about_details_icon" style={{fontSize:"25px"}}/>;
-  if(props.type == "website")
-  return  <LanguageOutlinedIcon className="about_details_icon" style={{fontSize:"25px"}}/>;
-  if(props.type == "address")
-  return  <HomeOutlinedIcon className="about_details_icon" style={{fontSize:"25px"}}/>;
+function DetailType(props) {
+  if (props.type == "email")
+    return <MailOutlineRoundedIcon className="about_details_icon" style={{ fontSize: "25px" }} />;
+  if (props.type == "instagram")
+    return <InstagramIcon className="about_details_icon" style={{ fontSize: "25px" }} />;
+  if (props.type == "website")
+    return <LanguageOutlinedIcon className="about_details_icon" style={{ fontSize: "25px" }} />;
+  if (props.type == "address")
+    return <HomeOutlinedIcon className="about_details_icon" style={{ fontSize: "25px" }} />;
   else
-  return  <HomeOutlinedIcon className="about_details_icon" style={{fontSize:"25px"}}/>;
+    return <HomeOutlinedIcon className="about_details_icon" style={{ fontSize: "25px" }} />;
 }
-const aboutdetails = (props) => (
+const about_details = (props) => (
 
-<div className = "aboutdetails" onClick={()=> window.open(props.link, "_blank")} >
-
-  <DetailType type = {props.type}/>
-
- <p className="about_details_title"> &nbsp; { props.title} : </p>
- <p className="about_details_value" > &nbsp; {props.value}</p>
-                  
-</div>
-
+  <div className="about_details" onClick={() => window.open(props.link, "_blank")} >
+    <DetailType type={props.type} />
+    <p className="about_details_title"> &nbsp; {props.title} : </p>
+    <p className="about_details_value" > &nbsp; {props.value}</p>
+  </div>
 );
 
-export default aboutdetails;
+export default about_details;
